6d21dbd03ad09596d870bdda66ed4bd30ff9879f
Ransford Oppong <roppong32@st.knust.edu.gh>
2025-02-21 16:26:47 -0800

    google analytics setup

--- app/(router)/layout.js
+++ app/(router)/layout.js
@@ -1,4 +1,5 @@
 "use client";
+import Script from "next/script";
 import React from "react";
 import SideNav from "./_components/SideNav";
 import Header from "./_components/Header";
@@ -6,6 +7,19 @@ import Header from "./_components/Header";
 function layout({ children }) {
   return (
     <div>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-FH8BPBFD4W"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+          gtag("js", new Date());
+          gtag("config", "G-FH8BPBFD4W");
+          }`}
+      </Script>
       <div className="sm:w-64 hidden sm:block fixed">
         <SideNav />
       </div>
