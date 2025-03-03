a85fe41fd3e40d5f6622ef4e040083f9bc190331
Ransford Oppong <roppong32@st.knust.edu.gh>
2025-03-03 05:29:46 -0800

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -9,19 +9,21 @@ export default function RootLayout({ children }) {
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <html lang="en">
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-FH8BPBFD4W"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-          gtag("js", new Date());
-          gtag("config", "G-FH8BPBFD4W");
-          }`}
-        </Script>
+        <head>
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-FH8BPBFD4W"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+            gtag("config", "G-FH8BPBFD4W");
+          `}
+          </Script>
+        </head>
         <body>
           {children}
 
